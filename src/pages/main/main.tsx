105866c8e6f4a82233294408137602de2052faa8
jobearry <golimlim2209@gmail.com>
2025-04-03 23:39:46 +0800

    initial mobile responsive

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -3,16 +3,16 @@ import { Showcase } from "../../components/showcase/showcase"
 
 export const Main = () => {
   return (
-    <section className="h-dvh border grid grid-cols-3">
+    <section className="h-[100vh] border grid sm:grid-cols-3 grid-cols-1">
       <div className="border col-span-2 grid place-items-center overflow-y-scroll">
         <div className="absolute top-6">
           <Navbar></Navbar>
         </div>
-        <div className="h-dvh border w-full grid place-items-center">
+        <div className="h-full border w-full grid place-items-center">
           <Showcase></Showcase>
         </div>
       </div>
-      <div className="border m-4">
+      <div className="border m-4 sm:block hidden">
         <h1>Resume</h1>
       </div>
     </section>
